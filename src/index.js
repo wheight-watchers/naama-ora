86b7d118db00583ec7788e4e35692afed79c916e
OraBenesh <orabenesh@gmail.com>
2022-06-17 11:00:10 +0300

    initialize

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert('hello! delete me please :)');
+
+function doRequest(){
+    debugger
+    let xhr=new XMLHttpRequest ();
+    let url = new URL('https://google.com/search');
+    xhr.onload(()=>print("I'M ON LOADED"));
+    xhr.onprogress = function(event) {
+        if (event.lengthComputable) {
+          alert(`Received ${event.loaded} of ${event.total} bytes`);
+        } else {
+          alert(`Received ${event.loaded} bytes`); // no Content-Length
+        }
+      
+      };
+    xhr.open("GET",url);
+    xhr.send();
+}
