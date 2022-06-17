d42cdbbda3abccfac85dc0b48f446c13bcf274e6
OraBenesh <orabenesh@gmail.com>
2022-06-17 11:49:15 +0300

    define default branch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-
-function doRequest(){
-    debugger
-    let xhr=new XMLHttpRequest ();
-    let url = new URL('https://google.com/search');
-    xhr.onload(()=>print("I'M ON LOADED"));
-    xhr.onprogress = function(event) {
-        if (event.lengthComputable) {
-          alert(`Received ${event.loaded} of ${event.total} bytes`);
-        } else {
-          alert(`Received ${event.loaded} bytes`); // no Content-Length
-        }
-      
-      };
-      xhr.onerror = function() {
-        alert("Request failed");
-      };
-    xhr.open("GET",url);
-    xhr.send();
+function doRequest() {
+  debugger;
+  let xhr = new XMLHttpRequest();
+  let url = new URL("https://google.com/search");
+  xhr.onload(() => alert("I'M ON LOADED"));
+  xhr.onprogress = function (event) {
+    if (event.lengthComputable) {
+      alert(`Received ${event.loaded} of ${event.total} bytes`);
+    } else {
+      alert(`Received ${event.loaded} bytes`); // no Content-Length
+    }
+  };
+  xhr.onerror = function () {
+    alert("Request failed");
+  };
+  xhr.open("GET", url);
+  xhr.send();
 }
