8d87ca23bbad97cea6b92e2de5c046ae7f577d74
OraBenesh <orabenesh@gmail.com>
2022-06-19 13:56:39 +0300

    logIn

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-function doRequest() {
+// function doRequest() {
+//   debugger;
+//   let xhr = new XMLHttpRequest();
+//   let url = new URL("https://google.com/search");
+//   xhr.onload(() => alert("I'M ON LOADED"));
+//   xhr.onprogress = function (event) {
+//     if (event.lengthComputable) {
+//       alert(`Received ${event.loaded} of ${event.total} bytes`);
+//     } else {
+//       alert(`Received ${event.loaded} bytes`); // no Content-Length
+//     }
+//   };
+//   xhr.onerror = function () {
+//     alert("Request failed");
+//   };
+//   xhr.open("GET", url);
+//   xhr.send();
+// }
+function logIn() {
   debugger;
-  let xhr = new XMLHttpRequest();
-  let url = new URL("https://google.com/search");
-  xhr.onload(() => alert("I'M ON LOADED"));
-  xhr.onprogress = function (event) {
-    if (event.lengthComputable) {
-      alert(`Received ${event.loaded} of ${event.total} bytes`);
-    } else {
-      alert(`Received ${event.loaded} bytes`); // no Content-Length
-    }
-  };
-  xhr.onerror = function () {
-    alert("Request failed");
-  };
-  xhr.open("GET", url);
-  xhr.send();
-}
+  let mail = document.getElementById('mailInput').value;
+  let pswd = document.getElementById('passwordInput').value;
+  if (mail == 'manager@gmail.com' && pswd == '123') {
+      // window.location.assign('Actions.html')
+      if(window.confirm("welcome"))
+          {window.location.href='Manager.html'}   
+  }
+  else {window.location.href='User.html'}   
+};
