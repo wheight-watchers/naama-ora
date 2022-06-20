fb1da3397cd0e83ad70913662a6ad18c61000b8e
OraBenesh <orabenesh@gmail.com>
2022-06-20 21:10:07 +0300

    show users and theirs details

---
+++ src/functions.js
@@ -0,0 +1,74 @@
+const logIn = () => {
+  debugger;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "file.json");
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status != 200) {
+      alert(`Error ${xhr.status}: ${managerXHR.statusText}`);
+    } else {
+      let manager = JSON.parse(xhr.response).manager;
+      console.log(manager);
+      let mail = document.getElementById("mailInput").value;
+      let pswd = document.getElementById("passwordInput").value;
+      if (mail == manager.email && pswd == manager.password) {
+        if (window.confirm("welcome")) {
+          window.location.href = "Manager.html";
+        }
+      } else {
+        let users = JSON.parse(xhr.responseText).users;
+        console.log(users);
+        if (users.find((u) => u.email == mail))
+          window.location.href = "User.html";
+      }
+    }
+  };
+};
+function directMyDetails(user){
+  const nameContainer = document.querySelector(".userName");
+  nameContainer.innerHTML += user.firstName+" "+user.lastName;
+  const detailsContainer = document.querySelector(".userDetails");
+  detailsContainer.innerHTML +=`
+  <p>${"id: "+user.id}</p>
+  <p> ${"mail: "+user.email}</p>
+  <p>${"address: "+user.address.city+' '+user.address.street+' '+user.address.building}</p>
+  <p>${"start wheight: "+user.wheights.startWheight}</p>
+  <p> wheights per meetings: </p>+`
+  const wheighs=user.wheights.meetings;
+  wheighs.foreach(wheight=>{
+    detailsContainer.innerHTML +=
+    `<p>${user.wheights.meetings.date+" : "+user.wheights.meetings.wheight}</p>`
+  })
+  
+  window.location.assign("Details.html") 
+}
+const getUsersForManager = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "./file.json");
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status != 200) {
+      alert("Error ${xhr.status}: ${xhr.statusText}");
+    } else {
+      users = JSON.parse(XHR.responseText).users;
+      console.log(users);
+      let tablle = "";
+      const bmi=(user.wheight*uer.height)^2
+      let color;
+      if(bmi<50)
+        color=green;
+      else color =red
+      users.foreach((user) => {
+        table += `
+      <tr>
+      <th>${user.firstName + " " + user.lastName}</th>
+      <th class=${color}>${"BMI : "+bmi}</th>
+      <th onclick="directMyDetails(${user})">details</th>
+      </tr>
+      `;
+      });
+      const container = document.querySelector(".usersTable");
+      container.innerHTML += table;
+    }
+  };
+};
--- src/index.js
+++ src/index.js
@@ -16,39 +16,25 @@
 //   xhr.open("GET", url);
 //   xhr.send();
 // }
-let m;
-function reqListener() {
-  console.log(this.responseText);
-}
-function loadEnd(e) {
-  console.log(
-    "The transfer finished (although we don't know if it succeeded or not)."
-  );
-}
-function getManager() {
+
+// function reqListener() {
+//   console.log(this.responseText);
+// }
+// function loadEnd(e) {
+//   console.log(
+//     "The transfer finished (although we don't know if it succeeded or not)."
+//   );
+// }
+ function logIn() {
   debugger;
-  try {
-    if (xhr.status != 200) {
-      alert(`Error ${xhr.status}: ${xhr.statusText}`);
-    } else {
-       m = this.response;
-      alert(xhr.response);
-    }
-  } catch (err) {
-    // instead of onerror
-    alert("Request failed");
-  }
- 
-}
-async function logIn() {
   // const initialization = async () => {
-  let url = "http://localhost:3000/manager";
+  // let url = "http://localhost:3000/manager";
   // const res = await fetch(url);
   // const manager = await res.json();
 
   // let table = '';
   // users.forEach(user => {
-  //     table += ` 
+  //     table += `
   //     <tr>
   //         <th>${user.firstName + ' ' + user.lastName}</th>
   //         <th>${user.weight.historyWeight[users.length - 1] / Math.sqrt(user.hight)}</th>
@@ -58,53 +44,83 @@ async function logIn() {
   // const container = document.querySelector('.usersTable');
   // container.innerHTML += table;
   // }
+  let res;
   debugger;
   let mail = document.getElementById("mailInput").value;
   let pswd = document.getElementById("passwordInput").value;
   let managerXHR = new XMLHttpRequest();
-  managerXHR.addEventListener("loadend", getManager);
-  managerXHR.open("GET", url);
-  const res=managerXHR.send();
-  const manager=await res.json();
-  if (mail == manager && pswd == manager) {
-    if (window.confirm("welcome")) {
-      window.location.assign("Manager.html");
-      window.location.href = "Manager.html";
-    }
-  } else {
-  let xhr = new XMLHttpRequest();
-    // xhr.addEventListener("load", reqListener);
-    xhr.open("GET", "file.json/users");
-    // xhr.addEventListener("loadend", loadEnd);
-    xhr.send();
-    if (mail == "manager@gmail.com" && pswd == "123") {
-      window.location.assign("User.html");
-      window.location.href = "User.html";
-    }
-  }
-}
-function getUsersFormanager() {
-  let xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", reqListener);
-  xhr.open("GET", "file.json");
-  xhr.addEventListener("loadend", loadEnd);
-  xhr.send();
-}
-function request() {
-  let xhr = new XMLHttpRequest();
+  managerXHR.onload = function () {
+    debugger;
 
-  xhr.open("GET", "file.json");
-
-  try {
-    xhr.send();
-    if (xhr.status != 200) {
-      alert(`Error ${xhr.status}: ${xhr.statusText}`);
+    if (managerXHR.status != 200) {
+      alert(`Error ${managerXHR.status}: ${managerXHR.statusText}`);
     } else {
-      alert(xhr.response);
+     
+      let manag = JSON.parse(managerXHR.responseText);
+        console.log(manag);
+      
     }
-  } catch (err) {
-    // instead of onerror
-    alert("Request failed");
-  }
+  };
+  managerXHR.open('GET', './file.json');
+  managerXHR.send();
+  // try {
+  //   if (managerXHR.status != 200) {
+  //     debugger;
+  //     alert(`Error ${managerXHR.status}: ${managerXHR.statusText}`);
+  //   } else {
+  //     debugger;
+  //     res = JSON.parse(managerXHR.responseText);
+  //     // m = this.response;
+  //     alert(managerXHR.response);
+  //   }
+  // } catch (err) {
+  //   debugger;
+  //   // instead of onerror
+  //   alert("Request failed");
+  // }
 }
-request();
+
+//   const manager = await res.json();
+//   if (mail == manager && pswd == manager) {
+//     if (window.confirm("welcome")) {
+//       window.location.assign("Manager.html");
+//       window.location.href = "Manager.html";
+//     }
+//   } else {
+//     let xhr = new XMLHttpRequest();
+//     // xhr.addEventListener("load", reqListener);
+//     xhr.open("GET", "file.json/users");
+//     // xhr.addEventListener("loadend", loadEnd);
+//     xhr.send();
+//     if (mail == xhr && pswd == "123") {
+//       window.location.assign("User.html");
+//       window.location.href = "User.html";
+//     }
+//   }
+// }
+
+// function getUsersFormanager() {
+//   let xhr = new XMLHttpRequest();
+//   xhr.addEventListener("load", reqListener);
+//   xhr.open("GET", "file.json");
+//   xhr.addEventListener("loadend", loadEnd);
+//   xhr.send();
+// }
+// function request() {
+//   let xhr = new XMLHttpRequest();
+
+//   xhr.open("GET", "file.json");
+
+//   try {
+//     xhr.send();
+//     if (xhr.status != 200) {
+//       alert(`Error ${xhr.status}: ${xhr.statusText}`);
+//     } else {
+//       alert(xhr.response);
+//     }
+//   } catch (err) {
+//     // instead of onerror
+//     alert("Request failed");
+//   }
+// }
+// request();
