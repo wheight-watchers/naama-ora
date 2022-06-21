0633207c17e6078020d27bfafb8e592bfb23c510
OraBenesh <orabenesh@gmail.com>
2022-06-21 14:19:52 +0300

    user details

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+let CurrentUser;
 const logIn = () => {
   debugger;
   const xhr = new XMLHttpRequest();
@@ -65,28 +66,62 @@ const getUsersForManager = () => {
     if (xhr.status != 200) {
       alert("Error ${xhr.status}: ${xhr.statusText}");
     } else {
-      users = JSON.parse(xhr.responseText).users;
+      let users = JSON.parse(xhr.responseText).users;
       console.log(users);
-      let tablle = "";
+      let table = "";
       // const currentUser=localStorage.getItem("cu")
       let bmi;
-      // = (currentUser.wheighs.meetings[1].wheight * currentUser.height) ^ 2;
+      // = (currentUser.Wheighs.meetings[1].wheight/(currentUser.height* currentUser.height);
       let color;
 
-      users.foreach((user) => {
-        bmi = (user.wheights.meetings[1].wheight * user.height) ^ 2;
-        if (bmi < 50) color = green;
-        else color = red;
+      users.forEach((user) => {
+        bmi = (user.Wheights.meetings[1].wheight/(user.height* user.height));
+        if (bmi < 50) color = "green";
+        else color = "red";
         table += `
       <tr>
-      <th>${user.firstName + " " + user.lastName}</th>
-      <th class=${color}>${"BMI : " + bmi}</th>
-      <th onclick="directMyDetails(${user})">details</th>
+      <th>${user.firstName + " " + user.lastName}</th><br/>
+      <th class=${color}>${"CURRENT BMI : " + bmi}</th><br/>
+      <th>${"you started with BMI  : " +(user.Wheights.startWheight/(user.height* user.height))}</th>
       </tr>
-      `;
+      <button onclick="directMyDetails(${user})">details</button>
+      <br/>`
       });
       const container = document.querySelector(".usersTable");
       container.innerHTML += table;
     }
   };
 };
+function userDetails() {
+  debugger;
+  var myData = localStorage['cu'];
+  localStorage.clear();
+
+  var value1 = JSON.parse(myData).firstName
+  var value2 = JSON.parse(myData).lastName
+  var value3 = JSON.parse(myData).email
+  var value5 = JSON.parse(myData).address.city
+  var value6 = JSON.parse(myData).address.street
+  var value7 = JSON.parse(myData).address.building
+  var value8 = JSON.parse(myData).age
+  var value9= JSON.parse(myData).height
+  var value10=JSON.parse(myData).Weights.startWeight
+  var meet=JSON.parse(myData).Weights.meetings
+  console.log(meet)
+  meet.forEach(m=>{
+    document.getElementById("meeting").innerHTML+= "                          "
+
+    document.getElementById("meeting").innerHTML+= '     date:         '+  m.date
+    document.getElementById("meeting").innerHTML+= "                          "
+    document.getElementById("meeting").innerHTML+='      weight:        '+m.weight
+    document.getElementById("meeting").innerHTML+=`</br>`
+
+  })
+//  JSON.parse(myData).meetings.forEach(x=> document.getElementById("meeting").innerHTML+=x.weighs.meetings.date)
+  document.getElementById("user").innerHTML+=value1
+  document.getElementById("name").innerHTML=value1+' '+value2
+  document.getElementById("email").innerHTML =value3
+  document.getElementById("height&age").innerHTML=value8+' '+value9
+  document.getElementById("address").innerHTML=value6+' '+value7+' '+value5;
+  document.getElementById("StartingWeight").innerHTML=value10
+}
