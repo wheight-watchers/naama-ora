4e5d2d07bc505f777e1875659ed6a840ce112a82
OraBenesh <orabenesh@gmail.com>
2022-06-21 13:42:07 +0300

    add css

--- src/functions.js
+++ src/functions.js
@@ -18,125 +18,43 @@ const logIn = () => {
       } else {
         let users = JSON.parse(xhr.responseText).users;
         console.log(users);
-        if (users.find(u=> u.email == mail)!=null)
+        CurrentUser = users.find((u) => u.email == mail);
+        if (CurrentUser != null) {
+          debugger;
+          console.log(CurrentUser);
+          localStorage.setItem("cu", JSON.stringify(CurrentUser));
           window.location.href = "src/User.html";
+        }
       }
     }
   };
 };
 
-
-
-// function directMyDetails(user) {
-//   debugger;
-//   const nameContainer = document.querySelector(".userName");
-//   nameContainer.innerHTML += user.firstName + " " + user.lastName;
-//   const detailsContainer = document.querySelector(".userDetails");
-//   detailsContainer.innerHTML += `
-//   <p>${"id: " + user.id}</p>
-//   <p> ${"mail: " + user.email}</p>
-//   <p>${"address: " + user.address.city + ' ' + user.address.street + ' ' + user.address.building}</p>
-//   <p>${"start weight: " + user.weights.startWeight}</p>
-//   <p> weights per meetings: </p>+`
-//   const weighs = user.weights.meetings;
-//   weighs.foreach(weight => {
-//     detailsContainer.innerHTML +=
-//       `<p>${user.weights.meetings.date + " : " + user.weights.meetings.weight}</p>`
-//   })
-
-//   window.location.assign("Details.html")
-// }
-// const getUsersForManager = () => {
-//   debugger;
-//   const xhr = new XMLHttpRequest();
-
-//   xhr.open("GET", "./db.json");
-//   xhr.send();
-//   xhr.onload = () => {
-//     if (xhr.status != 200) {
-//       alert(`Error ${xhr.status}: ${xhr.statusText}`);
-//     }
-//     else {
-//       let users = JSON.parse(xhr.responseText).users;
-//       console.log(users)
-//       let table = '';
-//       const bmi = (users.weight * users.height) ^ 2
-//       let color;
-//       let green;
-//       let red;
-//       if (bmi < 50)
-//         color = green;
-//       else color = red
-//       users.forEach(user => {
-//         table += `
-//          <tr>
-//         <th>${user.firstName + " " + user.lastName}</th>
-//         <th class=${color}>${"BMI : " + bmi}</th>
-//         </tr>
-//         <button onclick="directMyDetails({user})"> details</button>
-//         <br/>
-//         `
-//         // <button {onclick="directMyDetails(${user})"}>details</button>
-//         // <button {onclick="directMyDetails(${user})"}>details</button>
-//       })
-//       const container = document.querySelector(".usersTable");
-//       container.innerHTML += table;
-//     }
-//   };
-// };
-// function userDetails() {
-//   debugger;
-//   var myData = localStorage['cu'];
-//   localStorage.clear();
-
-//   var value1 = JSON.parse(myData).firstName
-//   var value2 = JSON.parse(myData).lastName
-//   var value3 = JSON.parse(myData).email
-//   var value5 = JSON.parse(myData).address.city
-//   var value6 = JSON.parse(myData).address.street
-//   var value7 = JSON.parse(myData).address.building
-//   var value8 = JSON.parse(myData).age
-//   var value9= JSON.parse(myData).height
-//   var value10=JSON.parse(myData).Weights.startWeight
-//   var meet=JSON.parse(myData).Weights.meetings
-//   console.log(meet)
-//   meet.forEach(m=>{
-//     document.getElementById("meeting").innerHTML+= "                          "
-
-//     document.getElementById("meeting").innerHTML+= '     date:         '+  m.date
-//     document.getElementById("meeting").innerHTML+= "                          "
-//     document.getElementById("meeting").innerHTML+='      weight:        '+m.weight
-//     document.getElementById("meeting").innerHTML+=`</br>`
-
-//   })
-// //  JSON.parse(myData).meetings.forEach(x=> document.getElementById("meeting").innerHTML+=x.weighs.meetings.date)
-//   document.getElementById("user").innerHTML+=value1
-//   document.getElementById("name").innerHTML=value1+' '+value2
-//   document.getElementById("email").innerHTML =value3
-//   document.getElementById("height&age").innerHTML=value8+' '+value9
-//   document.getElementById("address").innerHTML=value6+' '+value7+' '+value5;
-//   document.getElementById("StartingWeight").innerHTML=value10
-// }
-
-
-
-function directMyDetails(user){
+function directMyDetails(user) {
   const nameContainer = document.querySelector(".userName");
-  nameContainer.innerHTML += user.firstName+" "+user.lastName;
+  nameContainer.innerHTML += user.firstName + " " + user.lastName;
   const detailsContainer = document.querySelector(".userDetails");
-  detailsContainer.innerHTML +=`
-  <p>${"id: "+user.id}</p>
-  <p> ${"mail: "+user.email}</p>
-  <p>${"address: "+user.address.city+' '+user.address.street+' '+user.address.building}</p>
-  <p>${"start wheight: "+user.wheights.startWheight}</p>
-  <p> wheights per meetings: </p>+`
-  const wheighs=user.wheights.meetings;
-  wheighs.foreach(wheight=>{
-    detailsContainer.innerHTML +=
-    `<p>${user.wheights.meetings.date+" : "+user.wheights.meetings.wheight}</p>`
-  })
-  
-  window.location.assign("src/Details.html") 
+  detailsContainer.innerHTML += `
+  <p>${"id: " + user.id}</p>
+  <p> ${"mail: " + user.email}</p>
+  <p>${
+    "address: " +
+    user.address.city +
+    " " +
+    user.address.street +
+    " " +
+    user.address.building
+  }</p>
+  <p>${"start wheight: " + user.wheights.startWheight}</p>
+  <p> wheights per meetings: </p>+`;
+  const wheighs = user.wheights.meetings;
+  wheighs.foreach((wheight) => {
+    detailsContainer.innerHTML += `<p>${
+      user.wheights.meetings.date + " : " + user.wheights.meetings.wheight
+    }</p>`;
+  });
+
+  window.location.assign("src/Details.html");
 }
 const getUsersForManager = () => {
   debugger;
@@ -150,16 +68,19 @@ const getUsersForManager = () => {
       users = JSON.parse(xhr.responseText).users;
       console.log(users);
       let tablle = "";
-      const bmi=(user.wheight*user.height)^2
+      // const currentUser=localStorage.getItem("cu")
+      let bmi;
+      // = (currentUser.wheighs.meetings[1].wheight * currentUser.height) ^ 2;
       let color;
-      if(bmi<50)
-        color=green;
-      else color =red
+
       users.foreach((user) => {
+        bmi = (user.wheights.meetings[1].wheight * user.height) ^ 2;
+        if (bmi < 50) color = green;
+        else color = red;
         table += `
       <tr>
       <th>${user.firstName + " " + user.lastName}</th>
-      <th class=${color}>${"BMI : "+bmi}</th>
+      <th class=${color}>${"BMI : " + bmi}</th>
       <th onclick="directMyDetails(${user})">details</th>
       </tr>
       `;
