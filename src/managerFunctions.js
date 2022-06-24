b110d90caf225b39fce8ec848f75183d50dad8a5
OraBenesh <orabenesh@gmail.com>
2022-06-24 11:51:24 +0300

    buttons design

--- src/managerFunctions.js
+++ src/managerFunctions.js
@@ -176,41 +176,6 @@ function showUsers(jsonusers, numOfmeetings) {
   start = 2;
   // return container;
 }
-function userDetails() {
-  debugger;
-  var myData = localStorage["cu"];
-  // localStorage.clear();
-  var value1 = JSON.parse(myData).firstName;
-  var value2 = JSON.parse(myData).lastName;
-  var value3 = JSON.parse(myData).email;
-  var value5 = JSON.parse(myData).address.city;
-  var value6 = JSON.parse(myData).address.street;
-  var value7 = JSON.parse(myData).address.building;
-  var value8 = JSON.parse(myData).age;
-  var value9 = JSON.parse(myData).height;
-  var value10 = JSON.parse(myData).Wheights.startWheight;
-  var meet = JSON.parse(myData).Wheights.meetings;
-  console.log(meet);
-  meet.forEach((m) => {
-    debugger;
-    document.getElementById("meeting").innerHTML +=
-      "     date:         " + m.date;
-    document.getElementById("meeting").innerHTML +=
-      "                          ";
-    document.getElementById("meeting").innerHTML +=
-      "      weight:        " + m.wheight;
-    document.getElementById("meeting").innerHTML += `</br>`;
-  });
-
-  document.getElementById("user").innerHTML += value1;
-  document.getElementById("name").innerHTML = value1 + " " + value2;
-  document.getElementById("email").innerHTML = value3;
-  document.getElementById("height").innerHTML = value9;
-  document.getElementById("age").innerHTML = value8;
-  document.getElementById("address").innerHTML =
-    value6 + " " + value7 + " " + value5;
-  document.getElementById("StartingWeight").innerHTML = value10;
-}
 function directMyDetails(user) {
   debugger;
   window.location.href = `Details.html?id=${user.id}`;
@@ -352,5 +317,8 @@ function filterByAddress(users, citySelect, streetSelect) {
     })
   }
   return users;
+}
+function newMeeting(){
+  
 }
 // window.onload=getUsersForManager();
