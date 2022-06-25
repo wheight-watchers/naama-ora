90ac7d8252573d2ecdc17ec9575fd76056e36384
naamafrank1 <naama0556772275@gmail.com>
2022-06-26 02:32:58 +0300

    not important

--- src/userFunctions.js
+++ src/userFunctions.js
@@ -10,12 +10,12 @@ function userDetails() {
   var value7 = JSON.parse(myData).address.building;
   var value8 = JSON.parse(myData).age;
   var value9 = JSON.parse(myData).height;
-  var value10 = JSON.parse(myData).Wheights.startWheight;
-  var meet = JSON.parse(myData).Wheights.meetings;
+  var value10 = JSON.parse(myData).Weights.startWeight;
+  var meet = JSON.parse(myData).Weights.meetings;
   console.log(meet);
   meet.forEach((m) => {
     debugger;
-saveInVariableTheDetail
+   
     document.getElementById("meeting").innerHTML +=
       "     date:         " + m.date;
     document.getElementById("meeting").innerHTML +=
@@ -76,45 +76,47 @@ function saveYourDetails() {
   var age=document.getElementById("ageInput").value
   var height=document.getElementById("heightInput").value
 
+  alert("your detail update in variable:(")
 
-    var myData = localStorage["cu"];
-    // localStorage.clear();
-    var value1 = JSON.parse(myData).firstName;
-    var value2 = JSON.parse(myData).lastName;
-    var value3 = JSON.parse(myData).email;
-    var value5 = JSON.parse(myData).address.city;
-    var value6 = JSON.parse(myData).address.street;
-    var value7 = JSON.parse(myData).address.building;
-    var value8 = JSON.parse(myData).age;
-    var value9 = JSON.parse(myData).height;
-    var value10 = JSON.parse(myData).Weights.startWeight;
-    var meet = JSON.parse(myData).Weights.meetings;
-    console.log(meet);
-    meet.forEach((m) => {
-      debugger;
-      document.getElementById("meeting").innerHTML +=
-        "     date:         " + m.date;
-      document.getElementById("meeting").innerHTML +=
-        "                          ";
-      document.getElementById("meeting").innerHTML +=
-        "      weight:        " + m.weight;
-      document.getElementById("meeting").innerHTML += `</br>`;
-    });
+  window.location.href = "User.html";
+    // var myData = localStorage["cu"];
+    // // localStorage.clear();
+    // var value1 = JSON.parse(myData).firstName;
+    // var value2 = JSON.parse(myData).lastName;
+    // var value3 = JSON.parse(myData).email;
+    // var value5 = JSON.parse(myData).address.city;
+    // var value6 = JSON.parse(myData).address.street;
+    // var value7 = JSON.parse(myData).address.building;
+    // var value8 = JSON.parse(myData).age;
+    // var value9 = JSON.parse(myData).height;
+    // var value10 = JSON.parse(myData).Weights.startWeight;
+    // var meet = JSON.parse(myData).Weights.meetings;
+    // console.log(meet);
+    // meet.forEach((m) => {
+    //   debugger;
+    //   document.getElementById("meeting").innerHTML +=
+    //     "     date:         " + m.date;
+    //   document.getElementById("meeting").innerHTML +=
+    //     "                          ";
+    //   document.getElementById("meeting").innerHTML +=
+    //     "      weight:        " + m.weight;
+    //   document.getElementById("meeting").innerHTML += `</br>`;
+    // });
   
-    document.getElementById("user").innerHTML += value1;
-    document.getElementById("name").innerHTML = value1 + " " + value2;
-    document.getElementById("email").innerHTML = value3;
-    document.getElementById("height").innerHTML = value9;
-    document.getElementById("age").innerHTML = value8;
-    document.getElementById("address").innerHTML =
-      value6 + " " + value7 + " " + value5;
-    document.getElementById("StartingWeight").innerHTML = value10;
+    // document.getElementById("user").innerHTML += value1;
+    // document.getElementById("name").innerHTML = value1 + " " + value2;
+    // document.getElementById("email").innerHTML = value3;
+    // document.getElementById("height").innerHTML = value9;
+    // document.getElementById("age").innerHTML = value8;
+    // document.getElementById("address").innerHTML =
+    //   value6 + " " + value7 + " " + value5;
+    // document.getElementById("StartingWeight").innerHTML = value10;
   }
   function editdetails(){
     window.location.href = "editUser.html";
     debugger;
   }
-  function edit(){
+  async function edit(){
     debugger;
     var myData = localStorage["cu"];
     // localStorage.clear();
