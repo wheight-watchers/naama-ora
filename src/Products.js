4e2f0f67c6d52fb7ee56365d8cd4aed0cfb9fc96
naamafrank1 <naama0556772275@gmail.com>
2022-06-29 03:19:43 +0300

    autocomplete option

--- src/Products.js
+++ src/Products.js
@@ -1,6 +1,6 @@
 function searchProducts() {
   debugger;
-  document.getElementById('load').style.display='block'
+  document.getElementById('load').style.display = 'block'
   document.getElementById("ingredients").innerHTML = ""
   const options = {
     method: "GET",
@@ -18,7 +18,7 @@ function searchProducts() {
       return data;
     })
     .then((data) => {
-    
+
       data = data.filter((product) => {
         return product.shmmitzrach.includes(productname)
       });
@@ -26,25 +26,119 @@ function searchProducts() {
     }).then((data) => {
       if (data.length == 0) {
         alert("There is no such product")
-        document.getElementById('load').style.display='none'
+        document.getElementById('load').style.display = 'none'
       }
       else {
-        document.getElementById('load').style.display='none'
+        document.getElementById('load').style.display = 'none'
         console.log(data)
         data.forEach((d) => {
           document.getElementById("ingredients").innerHTML +=
-          `<h1>${d.shmmitzrach}</h1>` +
-          `<h3>total fat:${d.total_fat}</h3>` +
-          `<h3>total sugars :${d.total_sugars}</h3>` +
-          `<h3> carbohydrates :${d.carbohydrates}</h3>` +
-          `<h3> food energy :${d.food_energy}</h3>` +
-          `<h3>poly unsaturated fat :${d.poly_unsaturated_fat}</h3>`
-          ; debugger
+            `<h1>${d.shmmitzrach}</h1>` +
+            `<h3>total fat:${d.total_fat}</h3>` +
+            `<h3>total sugars :${d.total_sugars}</h3>` +
+            `<h3> carbohydrates :${d.carbohydrates}</h3>` +
+            `<h3> food energy :${d.food_energy}</h3>` +
+            `<h3>poly unsaturated fat :${d.poly_unsaturated_fat}</h3>`
+            ; debugger
         })
       }
     })
     .catch((err) => console.log(err));
 }
-function clearProducts(){
-  document.getElementById("ingredients").innerHTML=""
+
+function clearProducts() {
+
+  document.getElementById("ingredients").innerHTML = ""
 }
+let arrProductsName = []
+function CreateArrayOfAllProductsName() {
+  i = 0;
+  index = 0
+  debugger;
+
+
+  const options = {
+    method: "GET",
+    headers: {},
+  };
+
+  let result = fetch(
+    "https://data.gov.il/api/3/action/datastore_search?resource_id=c3cb0630-0650-46c1-a068-82d575c094b2&limit=4630",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const data = response.result.records;
+      console.log(data);
+      return data;
+    }).then((data) => {
+      data.forEach((d) => {
+       
+        arrProductsName.push(d.shmmitzrach)
+      })
+      debugger;
+      console.log(arrProductsName.length)
+      debugger
+      localStorage.setItem("arrProductsName", JSON.stringify(arrProductsName));
+      document.getElementById("buttonEdit").style.display="block"
+      
+    })
+}
+async function AutomaticSearchResults() {
+  arrProductsName=[]
+  debugger;
+  arrProductsName=  JSON.parse(localStorage.getItem("arrProductsName"))
+  //localStorage["arrProductsName"];
+ 
+  document.getElementById("resultAutomatic").innerHTML = ""
+  const inputValue = document.getElementById("productName").value
+ 
+  console.log(arrProductsName.length)
+
+  debugger;
+  
+  for (i = 0; i < 500; i++) {
+    debugger;
+    if (arrProductsName[i].includes(inputValue) == true) {
+      document.getElementById("resultAutomatic").innerHTML +=
+       `<button id="b+${i}" onclick="valueToInput(this.id)">${arrProductsName[i]}</button>`+
+      `<br></br>`
+    }
+  }
+
+}
+
+function valueToInput(val){
+  debugger
+  console.log(val)
+  debugger;
+ document.getElementById("productName").value=""
+debugger;
+v=document.getElementById(val).innerText
+document.getElementById("productName").value=v
+document.getElementById("resultAutomatic").innerHTML=""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
