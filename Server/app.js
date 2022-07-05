5b9f4f458beac62a62974a2be8b4b0138d86dad6
OraBenesh <orabenesh@gmail.com>
2022-07-05 17:11:15 +0300

    meeting router

--- Server/app.js
+++ Server/app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 // const cors = require("cors");
 const userRouter = require("./Routes/user.router");
-// const meetingRouter = require("./Routes/meeting.router");
+const meetingRouter = require("./Routes/meeting.router");
 const port = process.env.port|| 3000;
 
 // app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
-// app.use("/meeting"
-// // , authMiddleware
-// , meetingRouter);
+app.use("/meeting"
+, authMiddleware
+, meetingRouter);
 
 app.listen(port, () => {
   console.log(`process on port ${port}`);
