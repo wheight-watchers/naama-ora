8566031236d972e3cc19705f0986b6c5439d0b5e
naamafrank1 <naama0556772275@gmail.com>
2022-07-05 17:46:06 +0300

    diary servise and change to async

--- Server/Controllers/user.controller.js
+++ Server/Controllers/user.controller.js
@@ -56,23 +56,6 @@ module.exports.removeUser= async (req, res, next) => {
         next(err)
     }
 }
-// router.get('/getUsers', async (req, res) => {
-// });
-// router.get('/signIn', async (req, res) => {
-//     //return a specific user
-// });
-// router.post('/signUp', async (req, res) => {
-//     //add a new user
-//     UserService.addUser(
-//         // req.params.name,req.params.email,req.params.password
-//         );
-// });
-// router.put('/updateDetails', async (req, res) => {
-//     //update user details
-// });
-// router.delete('/deleteUser', async (req, res) => {
-//     //remove a user
-// });
 
 
 module.exports = router;
---
+++ Server/Services/diary.service.js
@@ -0,0 +1,18 @@
+
+async function getDiary(userId){
+    
+}
+
+async function addDiary(userId,diary){
+
+}
+
+async function updateDiary(userId,dairyId,dairy){
+
+}
+async function deleteDairy(userId,dairyId){
+
+}
+module.exports={
+   getDiary, addDiary,updateDiary,deleteDairy
+}
